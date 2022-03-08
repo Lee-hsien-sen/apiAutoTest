ff4800b6ae6e8b8d2837ced74ce3db11a4224c20
zeng.li <zeng.li@matrx.team>
2022-03-08 14:37:29 +0800

    userAccountIdByOther= "1-UA-148216587417591808"

--- src/com/qingzi/process/QZ.java
+++ src/com/qingzi/process/QZ.java
@@ -22,8 +22,9 @@ public class QZ extends BasicsGM{
 	public static String enterprise_Id = "1-EP-48824092482932736";//测试环境 企业id
 	public static String s_UserToken;//usertoken  getToken接口返回
 //	public static String userAccountId = "1-UA-119622539896897536";//开发环境用户 userAccountId  getToken接口返回
-	public static String userAccountId = "1-UA-137252758432178176";//测试环境用户 userAccountId  getToken接口返回
-	public static String userAccountIdByOther;//userAccountId  getToken接口返回
+	public static String userAccountId = "1-UA-155000601285533696";//测试环境用户 userAccountId  getToken接口返回
+	public static String hostNickName = "yxw@qq.com";//开发环境用户 发起者用户昵称
+	public static String userAccountIdByOther= "1-UA-148216587417591808";//userAccountId  getToken接口返回
 	public static String meeting_Id; //会议id   createMeeting接口返回
 	public static String m_Id; //会议短id   createMeeting接口返回
 	public static String userId; //主持人id   createMeeting接口返回
